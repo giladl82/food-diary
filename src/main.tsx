020b138e9f52de6a8b76e23a8d29e3a8ca26791d
Gilad Lev-Ari <giladl82@gmail.com>
2025-06-27 10:28:57 +0300

    Set for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then(
+    navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).then(
       (registration) => {
         console.log('Service Worker registered:', registration);
       },
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/food-diary/', // Set the base path for the application
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
